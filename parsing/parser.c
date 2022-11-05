d0c88e5f5c141d79174a12d2e9c52e34f4324bd0
Mohammed taha Meaizi <momeaizi@e1r11p1.1337.ma>
2022-11-05 17:02:22 +0100

    done

--- parsing/parser.c
+++ parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: momeaizi <momeaizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 13:11:38 by momeaizi          #+#    #+#             */
-/*   Updated: 2022/11/05 12:13:37 by momeaizi         ###   ########.fr       */
+/*   Updated: 2022/11/05 16:57:29 by momeaizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@ void	parser(t_world *world, t_camera *c, char *line, int fd)
 
 	while (line)
 	{
-		token = get_token(line, ' ', ft_strlen(line));
-		len = ft_strlen(token);
+		len = ft_strlen(line);
+		token = get_token(line, ' ', len);
 		if (!ft_strcmp(token, "A"))
-			parse_ambient(world, line + len + 1, ft_strlen(line + len + 1));
+			parse_ambient(world, line, len);
 		else if (!ft_strcmp(token, "C"))
-			parse_camera(c, line + len + 1, ft_strlen(line + len + 1));
+			parse_camera(c, line, len);
 		else if (!ft_strcmp(token, "L"))
-			parse_light(world, line + len + 1, ft_strlen(line + len + 1));
+			parse_light(world, line, len);
 		else if (!ft_strcmp(token, "sp"))
-			parse_sphere(world, line + len + 1, ft_strlen(line + len + 1));
+			parse_sphere(world, line, len);
 		else if (!ft_strcmp(token, "cy"))
-			parse_cylinder(world, line + len + 1, ft_strlen(line + len + 1));
+			parse_cylinder(world, line, len);
 		else if (!ft_strcmp(token, "pl"))
-			parse_plan(world, line + len + 1, ft_strlen(line + len + 1));
+			parse_plan(world, line, len);
 		else
 			error(token, "invalid identifier!\n");
 		free(line);
